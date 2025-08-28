1fe60cd3b67f48fc3dfe889733df177c173749ed
Timur Bakiev <tnbakiev@edu.hse.ru>
2025-08-29 01:19:43 +0300

    Added a basic tool to find and plot fixed points

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from typing import Tuple
 from numpy.typing import NDArray
-from src.plotting import plot_dynamics
+from src.plotting import plot_dynamics, plot_fixed_points, make_grid
 
 # type the formulas for the x and y components of the vector fields
 def V(x: NDArray, y: NDArray) -> Tuple[NDArray, NDArray]:
     return  np.ones_like(x), y**2 / (1. + x**2)
 
 if __name__ == "__main__":
-    plot_dynamics(V)
+
+    grid = make_grid()
+
+    fig, ax = plt.subplots()
+    ax.grid()
+
+    plot_dynamics(ax, V, grid)
+    plot_fixed_points(ax, V)
+
+    ax.set_aspect('equal')
+    plt.show()
--- src/plotting.py
+++ src/plotting.py
@@ -1,13 +1,18 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from typing import Callable, Tuple
+from matplotlib.axes import Axes
 from numpy.typing import NDArray
+from scipy.optimize import fsolve
 
-def plot_dynamics(vector_field: Callable[[NDArray, NDArray], Tuple[NDArray, NDArray]],
-    x_left: int = -2, x_right: int = 4, x_res: int = 100, y_down: int = -4, y_up: int = 2, y_res: int = 100) -> None:
+def make_grid(x_left: int = -2, x_right: int = 4, x_res: int = 100, y_down: int = -4, y_up: int = 2, y_res: int = 100) -> Tuple[NDArray, NDArray]:
 
     x, y = np.meshgrid(np.linspace(x_left, x_right, x_res), np.linspace(y_down, y_up, y_res))
 
+    return x, y
+
+def plot_dynamics(ax: Axes, vector_field: Callable[[NDArray, NDArray], Tuple[NDArray, NDArray]], grid: Tuple[NDArray, NDArray]) -> None:
+
+    x, y = grid
     vx, vy = vector_field(x, y)
 
     if type(vx) != object:
@@ -15,8 +20,14 @@ def plot_dynamics(vector_field: Callable[[NDArray, NDArray], Tuple[NDArray, NDAr
     if type(vy) != object:
         vy = vy * np.ones(x.shape, dtype=float)
 
-    fig, ax = plt.subplots()
-    plt.grid()
     ax.streamplot(x, y, vx, vy)
-    ax.set_aspect('equal')
-    plt.show()
+
+def plot_fixed_points(ax: Axes, vector_field: Callable[[NDArray, NDArray], Tuple[NDArray, NDArray]]) -> None:
+
+    def my_function(arg):
+        x, y = arg[0], arg[1]
+        return vector_field(x, y)
+
+    zero, _, _, _ = fsolve(my_function, [0, 0], full_output=True)
+    zero_x, zero_y = zero[0], zero[1]
+    ax.scatter(zero_x, zero_y, color='red', marker='o', label='Zeros')
