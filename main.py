c0d4ffea18d17bdfed34ea73d9959bd296488dd0
Timur Bakiev <tnbakiev@edu.hse.ru>
2025-08-29 13:54:46 +0300

    Added a basic tool to plot direction fields and a simple interface allowing to store images in .svg format

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from typing import Tuple
 from numpy.typing import NDArray
-from src.plotting import plot_dynamics, plot_fixed_points, make_grid
+from src.plotting import (plot_dynamics, plot_fixed_points, make_grid, plot_directionfield)
 
 # type the formulas for the x and y components of the vector fields
 def V(x: NDArray, y: NDArray) -> Tuple[NDArray, NDArray]:
@@ -10,13 +10,25 @@ def V(x: NDArray, y: NDArray) -> Tuple[NDArray, NDArray]:
 
 if __name__ == "__main__":
 
-    grid = make_grid()
+    grid = make_grid(x_res=25, y_res=25)
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(16 , 12))
     ax.grid()
 
-    plot_dynamics(ax, V, grid)
-    plot_fixed_points(ax, V)
+    plot_dynamics(ax, V, grid, density=1., minlength=1.0)
+    plot_directionfield(ax, V, grid)
+    # plot_fixed_points(ax, V)
 
     ax.set_aspect('equal')
     plt.show()
+
+    print("Do you want to save the plot? [yes|no]")
+    answer = input().lower()
+    if answer == "yes":
+        print("Enter the file name:")
+        filename = input()
+        fig.savefig(filename+'.svg', format='svg')
+    else:
+        print("Okay, plot not saved.")
+
+    print("Thank you for using the program.")
--- src/plotting.py
+++ src/plotting.py
@@ -10,17 +10,23 @@ def make_grid(x_left: int = -2, x_right: int = 4, x_res: int = 100, y_down: int 
 
     return x, y
 
-def plot_dynamics(ax: Axes, vector_field: Callable[[NDArray, NDArray], Tuple[NDArray, NDArray]], grid: Tuple[NDArray, NDArray]) -> None:
+def normalize_field(vx: NDArray, vy: NDArray, epsilon: float = 10e-8) -> Tuple[NDArray, NDArray]:
+
+    norms = np.sqrt(vx**2 + vy**2)
+    mask = (norms <= epsilon)
+    norms[mask] = 1
+    vx /= norms
+    vy /= norms
+
+    return vx, vy
+
+def plot_dynamics(ax: Axes, vector_field: Callable[[NDArray, NDArray], Tuple[NDArray, NDArray]], grid: Tuple[NDArray, NDArray],
+    minlength: float = 0.1, maxlength: float = 4.0, arrowstyle: str = '-', density: float | Tuple[float, float] = 1.0) -> None:
 
     x, y = grid
     vx, vy = vector_field(x, y)
 
-    if type(vx) != object:
-        vx = vx * np.ones(x.shape, dtype=float)
-    if type(vy) != object:
-        vy = vy * np.ones(x.shape, dtype=float)
-
-    ax.streamplot(x, y, vx, vy)
+    ax.streamplot(x, y, vx, vy, minlength=minlength, maxlength=maxlength, arrowstyle=arrowstyle, density=density, color='green')
 
 def plot_fixed_points(ax: Axes, vector_field: Callable[[NDArray, NDArray], Tuple[NDArray, NDArray]]) -> None:
 
@@ -31,3 +37,12 @@ def plot_fixed_points(ax: Axes, vector_field: Callable[[NDArray, NDArray], Tuple
     zero, _, _, _ = fsolve(my_function, [0, 0], full_output=True)
     zero_x, zero_y = zero[0], zero[1]
     ax.scatter(zero_x, zero_y, color='red', marker='o', label='Zeros')
+
+def plot_directionfield(ax: Axes, vector_field: Callable[[NDArray, NDArray], Tuple[NDArray, NDArray]], grid: Tuple[NDArray, NDArray]) -> None:
+
+    x, y = grid
+    vx, vy = normalize_field(*vector_field(x, y))
+
+    ax.quiver(x, y, vx, vy, color='blue', headwidth=0, headlength=0, headaxislength=0, pivot='mid', units='xy', minlength=1.0)
+    ax.scatter(x, y, color='blue', marker='o', s=20, alpha=1.)
+    ax.scatter(x, y, color='white', marker='o', s=12.5, alpha=1.)
